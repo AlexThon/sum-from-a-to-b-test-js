54fbdecef4a2d0e28976143de432017528614dd4
Alex Thon <ghaiquol@gmail.com>
2021-10-31 00:53:38 +0000

    Added sum function recursive version

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  // base case: a == b
+  
+  if(fromN === toN){
+    return fromN;
+  } while (fromN <= toN){
+    return fromN + sum(fromN+1,toN);
+  }
 }
 
+console.log(sum(2,7));
 module.exports = sum;
